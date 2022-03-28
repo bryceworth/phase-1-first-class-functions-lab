8402d103465916b01a8232c9e1569d316c03a34e
bryceworth <bryceaworth@gmail.com>
2022-03-28 14:01:43 -0700

    commit tree 122c62fc6bb249cc5b2244eb2b9ddc1e7f66b184

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(array){ return [...array.slice(0,2)]}
+
+const returnLastTwoDrivers = function(array){ return [...array.slice(2,4)]}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(fare){
+    return function(){
+        return fare * 5
+    }
+}
+
+function fareDoubler(fare2){
+    return fare2 * 2
+}
+
+function fareTripler(fare3){
+    return fare3 * 3
+}
+const selectDifferentDrivers = (arrayOfDrivers, returnFirstTwoDrivers) => {
+    return returnFirstTwoDrivers(arrayOfDrivers)
+    
+}
+
+const part1 = "hello";
+
+function demoChain(name) {
+  let part2 = "hi";
+  return function inner1() {
+    let part3 = "there";
+    return function inner2() {
+      // what variables are accessible here?
+    };
+  };
+}
+
+demoChain("Dr. Stephen Strange")()();
